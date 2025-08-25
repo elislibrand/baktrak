8da03b22f7f5d5c94c25aec4f5568adad80fc9c9
Elis Librand <elis.librand@gmail.com>
2025-08-25 20:47:35 +0200

    Add bouncing of audio stems and support for ignored stems

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 import sys
 import json
 import yt_dlp
+import logging
+from pydub import AudioSegment
+#from pydub.effects import normalize
 from audio_separator.separator import Separator
 
 ydl_opts = {
@@ -28,20 +31,40 @@ def download_from_yt(source):
 
 def split_into_stems():
     separator = Separator(
+        log_formatter = logging.Formatter('[%(module)s] %(message)s'),
         output_format = 'MP3',
         output_dir = 'tmp/',
     )
     separator.load_model(model_filename = 'htdemucs_6s.yaml')
 
     output_names = {
-        'Vocals': 'vocals',
-        'Drums': 'drums',
-        'Bass': 'bass',
-        'Other': 'other',
-        'Guitar': 'guitar',
-        'Piano': 'piano',
+        'Vocals': 'stem-vocals',
+        'Drums': 'stem-drums',
+        'Bass': 'stem-bass',
+        'Other': 'stem-other',
+        'Guitar': 'stem-guitar',
+        'Piano': 'stem-piano',
     }
     output_files = separator.separate('tmp/yt.mp3', output_names)
+
+def bounce_stems():
+    ms = len(AudioSegment.from_mp3('tmp/yt.mp3'))
+    mix = AudioSegment.silent(duration = ms)
+
+    ignored_stems = [
+        'stem-bass.mp3',
+        'stem-guitar.mp3',
+    ]
+
+    for f in os.listdir('tmp/'):
+        if f.lower().startswith('stem-') and f.lower().endswith('.mp3'):
+            if f in ignored_stems:
+                continue
+
+            stem = AudioSegment.from_mp3(f'tmp/{f}')
+            mix = mix.overlay(stem, position = 0)
+
+    mix.export('output/backing-track.mp3', format = 'mp3')
 
 def main():
     source = input('URL or search term: ')
@@ -59,16 +82,18 @@ def main():
         os.makedirs('output', exist_ok = True)
 
         download_from_yt(source)
+
         split_into_stems()
+        bounce_stems()
     finally:
         tmp_files = [
             'tmp/yt.mp3',
-            'tmp/vocals.mp3',
-            'tmp/drums.mp3',
-            'tmp/bass.mp3',
-            'tmp/other.mp3',
-            'tmp/guitar.mp3',
-            'tmp/piano.mp3',
+            'tmp/stem-vocals.mp3',
+            'tmp/stem-drums.mp3',
+            'tmp/stem-bass.mp3',
+            'tmp/stem-other.mp3',
+            'tmp/stem-guitar.mp3',
+            'tmp/stem-piano.mp3',
         ]
 
         for f in tmp_files:
